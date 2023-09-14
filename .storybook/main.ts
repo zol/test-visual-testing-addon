130aa328054aee113db7a187342ff9067865c613
Zoltan Olah <zol@hichroma.com>
2023-09-14 10:23:33 -0700

    Adds everything yay

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: "@chromaui/addon-visual-tests",
+
+      options: {
+        projectId: "Project:64f2300a8707b2c5276bab2c",
+        projectToken: "chpt_4e9587ad1e0d67d"
+      }
+    }
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
